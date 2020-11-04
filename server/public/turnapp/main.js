3b572e38be8efdfe53f282e4f8bfe4155f000c3e
Camilo Galvis <cgalvis@facturecolombia.com>
2020-11-04 11:39:32 -0500

    fix deploy 2

--- server/public/turnapp/main.js
+++ server/public/turnapp/main.js
@@ -898,7 +898,7 @@ class DialogEditTurnComponent {
 DialogEditTurnComponent.ɵfac = function DialogEditTurnComponent_Factory(t) { return new (t || DialogEditTurnComponent)(); };
 DialogEditTurnComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: DialogEditTurnComponent, selectors: [["turnapp-dialog-edit-turn"]], decls: 2, vars: 0, template: function DialogEditTurnComponent_Template(rf, ctx) { if (rf & 1) {
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "p");
-        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](1, "dialog-edit-turn works edit!");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](1, "dialog-edit-turn!");
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
     } }, styles: ["\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2NvbXBvbmVudHMvZGlhbG9nLWVkaXQtdHVybi9kaWFsb2ctZWRpdC10dXJuLmNvbXBvbmVudC5zY3NzIn0= */"] });
 /*@__PURE__*/ (function () { _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵsetClassMetadata"](DialogEditTurnComponent, [{
